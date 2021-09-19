3353c48d99ed700d5d4b8b623f92bf184a7ee4b5
BCole <bcolemutech@hotmail.com>
2021-09-19 18:23:59 -0500

    Handler for account creation (#35)
    * Cleanup Dockerfile
    
    * Test For handling user_register
    
    * Method to handle user_register
    
    * Register Action Hook
    
    * Fix test requirements
    
    * Fix linting error

--- src/includes/class-wp-vouched-verify.php
+++ src/includes/class-wp-vouched-verify.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpIncludeInspection */
 
 /**
  * The file that defines the core plugin class
@@ -173,6 +173,7 @@ class Wp_Vouched_Verify {
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
+        $this->loader->add_action('user_register', $plugin_public, 'handle_user_register');
 	}
 
 	/**
@@ -191,7 +192,8 @@ class Wp_Vouched_Verify {
 	 * @since     1.0.0
 	 * @return    string    The name of the plugin.
 	 */
-	public function get_plugin_name() {
+	public function get_plugin_name(): string
+    {
 		return $this->plugin_name;
 	}
 
@@ -201,7 +203,8 @@ class Wp_Vouched_Verify {
 	 * @since     1.0.0
 	 * @return    Wp_Vouched_Verify_Loader    Orchestrates the hooks of the plugin.
 	 */
-	public function get_loader() {
+	public function get_loader(): Wp_Vouched_Verify_Loader
+    {
 		return $this->loader;
 	}
 
@@ -211,7 +214,8 @@ class Wp_Vouched_Verify {
 	 * @since     1.0.0
 	 * @return    string    The version number of the plugin.
 	 */
-	public function get_version() {
+	public function get_version(): string
+    {
 		return $this->version;
 	}
 
