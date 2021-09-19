e390ecc5af9a0245a0ade2dcb35d52b3990b0d57
bcolemutech <bcolemutech@hotmail.com>
2021-09-19 18:01:06 -0500

    Test For handling user_register

---
+++ test/RegisterTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require '../src/public/class-wp-vouched-verify-public.php';
+
+class RegisterTest extends TestCase
+{
+
+    public function testUserRegisterShouldRun()
+    {
+        $pluginPublic = new Wp_Vouched_Verify_Public('Wp_Vouched_Verify', '1.0.0');
+        $_POST['email'] = "test";
+
+        $pluginPublic->handle_user_register(1);
+
+        $this->assertTrue(true);
+    }
+}
