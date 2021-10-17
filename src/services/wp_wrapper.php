b43cee3fbe4e70d58eb57d207c8f3b737bacf357
bcolemutech <bcolemutech@hotmail.com>
2021-10-16 20:07:03 -0500

    Get user meta with wrapper

--- src/services/wp_wrapper.php
+++ src/services/wp_wrapper.php
@@ -27,4 +27,9 @@ class wp_wrapper implements wp_wrapper_interface
     {
         return add_user_meta($user_id, $meta_key, $meta_value, $unique);
     }
+
+    public function get_user_meta(int $user_id, string $meta_key): mixed
+    {
+        return get_user_meta($user_id, $meta_key, true);
+    }
 }
--- src/services/wp_wrapper_interface.php
+++ src/services/wp_wrapper_interface.php
@@ -11,4 +11,6 @@ interface wp_wrapper_interface
     public function wp_remote_retrieve_body(array $response): string;
 
     public function add_user_meta(int $user_id, string $meta_key, $meta_value, bool $unique): bool;
+
+    public function get_user_meta(int $user_id, string $meta_key): mixed;
 }
