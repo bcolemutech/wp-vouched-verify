1749d18edfaf5d9cbc533530697c3db5dace5406
BCole <bcolemutech@hotmail.com>
2021-10-20 09:12:35 -0400

    Test to intercept login and get Invite ID from meta

--- test/LoginTest.php
+++ test/LoginTest.php
@@ -4,23 +4,28 @@
 use PHPUnit\Framework\TestCase;
 
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
+require_once dirname(__FILE__) . '/../wordpress/wp-includes/class-wp-user.php';
 require_once dirname(__FILE__) . '/../src/public/class-wp-vouched-verify-public.php';
 require_once dirname(__FILE__) . '/../src/services/wp_wrapper_interface.php';
 
 class LoginTest extends TestCase
 {
+
     public function testUserLoginShouldCheckForInviteId()
     {
+
         $stub = $this->getMockBuilder(wp_wrapper_interface::class)->getMock();
         $stub->method('get_option')->willReturn(array('url' => 'test.com', 'api_key' => 'abcd1234'));
 
-        $stub->expects($this->once())->method('get_user_meta')->will($this->returnValue('123'));
+		$stub->method('get_user_meta')->willReturn('123');
+        $stub->expects($this->once())->method('get_user_meta');
 
         $pluginPublic = new Wp_Vouched_Verify_Public('Wp_Vouched_Verify', '1.0.0', $stub);
 
-        $user = new WP_User(1,'John');
+        $user = $this->createStub(WP_User::class);
 
         $user->user_email = 'john@test.com';
+		$user->ID = 1;
 
         $pluginPublic->handle_wp_login('John', $user);
 
