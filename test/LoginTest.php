d4bc3e6acfa8d849fcbce767ee9b79ab7e2993eb
BCole <bcolemutech@hotmail.com>
2021-10-21 18:01:11 -0400

    Test for getting Invite from Vouched

--- test/LoginTest.php
+++ test/LoginTest.php
@@ -30,4 +30,25 @@ class LoginTest extends TestCase
         $pluginPublic->handle_wp_login('John', $user);
 
     }
+
+	public function testUserLoginShouldGetStatusOfInviteFromApi()
+	{
+
+		$stub = $this->getMockBuilder(wp_wrapper_interface::class)->getMock();
+		$stub->method('get_option')->willReturn(array('url' => 'test.com', 'api_key' => 'abcd1234'));
+
+		$stub->method('get_user_meta')->willReturn('123');
+		$stub->method('wp_remote_retrieve_body')->willReturn("{\"invite\":[{\"id\":\"123\",\"status\":\"completed\"}]}");
+		$stub->expects($this->once())->method('get_user_meta');
+		$stub->expects($this->once())->method('wp_remote_get');
+
+		$pluginPublic = new Wp_Vouched_Verify_Public('Wp_Vouched_Verify', '1.0.0', $stub);
+
+		$user = $this->createStub(WP_User::class);
+
+		$user->user_email = 'john@test.com';
+		$user->ID = 1;
+
+		$pluginPublic->handle_wp_login('John', $user);
+	}
 }
