65155af971a2413990ed513048464fb896937319
BCole <bcolemutech@hotmail.com>
2021-10-22 12:32:47 -0400

    Remove redundant test, expand setup for getting job

--- test/LoginTest.php
+++ test/LoginTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
@@ -9,27 +10,9 @@ require_once dirname(__FILE__) . '/../src/services/wp_wrapper_interface.php';
 
 class LoginTest extends TestCase
 {
-
-    public function testUserLoginShouldCheckForInviteId()
-    {
-	    $stub = $this->getWpStub('completed');
-	    $stub->expects($this->once())->method('get_user_meta');
-
-        $pluginPublic = new Wp_Vouched_Verify_Public('Wp_Vouched_Verify', '1.0.0', $stub);
-
-        $user = $this->createStub(WP_User::class);
-
-        $user->user_email = 'john@test.com';
-		$user->ID = 1;
-
-        $pluginPublic->handle_wp_login('John', $user);
-
-    }
-
-	public function testUserLoginShouldGetStatusOfInviteFromApi()
+	public function testUserLoginShouldGetStatusOfInviteFromApi_HappyPath()
 	{
-
-		$stub = $this->getWpStub('completed');
+		$stub = $this->getWpStub('completed', 'completed');
 		$stub->expects($this->once())->method('get_user_meta');
 		$stub->expects($this->once())->method('wp_remote_get')->with('test.com/api/invites/?id=123');
 
@@ -45,7 +28,7 @@ class LoginTest extends TestCase
 
 	public function testGivenInviteIsNotCompletedStoreMessage()
 	{
-		$stub = $this->getWpStub('accepted');
+		$stub = $this->getWpStub('accepted','completed');
 		$stub->expects($this->once())->method('get_user_meta');
 		$stub->expects($this->once())
 		     ->method('wp_remote_get')
@@ -65,16 +48,18 @@ class LoginTest extends TestCase
 	}
 
 	/**
-	 * @return mixed|\PHPUnit\Framework\MockObject\MockObject|wp_wrapper_interface
+	 * @return mixed|MockObject|wp_wrapper_interface
 	 */
-	public function getWpStub(string $inviteStatus) {
+	public function getWpStub(string $inviteStatus, string $jobStatus) {
 		$stub = $this->getMockBuilder( wp_wrapper_interface::class )->getMock();
 		$stub->method( 'get_option' )->willReturn( array( 'url' => 'test.com', 'api_key' => 'abcd1234' ) );
 
 		$stub->method( 'get_user_meta' )->willReturn( '123' );
-		$stub->method( 'wp_remote_retrieve_body' )
-		     ->willReturn( "{\"invite\":[{\"id\":\"123\",\"status\":\"" . $inviteStatus . "\"}]}" );
+		$stub->method( 'wp_remote_get' )->with('test.com/api/invites/?id=123')
+		     ->willReturn( array('body' => "{\"invite\":[{\"id\":\"123\",\"jobId\":\"5\",\"status\":\"" . $inviteStatus . "\"}]}" ));
 
+		$stub->method('wp_remote_get')->with('test.com/api/jobs/?id=5')
+			->willReturn(array('body'=>"{\"items\":[{\"id\":\"5\",\"status\":\"".$jobStatus."\"}]}"));
 		return $stub;
 	}
 }
