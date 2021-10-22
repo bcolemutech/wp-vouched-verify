bb2463e9239a13907693d915621608b3ad066bb2
BCole <bcolemutech@hotmail.com>
2021-10-22 12:33:12 -0400

    Remove get body from wrapper

--- src/services/wp_wrapper.php
+++ src/services/wp_wrapper.php
@@ -20,10 +20,6 @@ class wp_wrapper implements wp_wrapper_interface {
 		return wp_remote_retrieve_response_code( $response );
 	}
 
-	public function wp_remote_retrieve_body( array $response ): string {
-		return wp_remote_retrieve_body( $response );
-	}
-
 	public function add_user_meta( int $user_id, string $meta_key, $meta_value, bool $unique ): bool {
 		return add_user_meta( $user_id, $meta_key, $meta_value, $unique );
 	}
