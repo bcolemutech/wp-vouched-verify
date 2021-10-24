51f5108728255b569c6faa9bd6e81ee7a2815cdb
bcolemutech <bcolemutech@hotmail.com>
2021-10-23 22:19:03 -0500

    formatting #minor

--- src/public/class-wp-vouched-verify-public.php
+++ src/public/class-wp-vouched-verify-public.php
@@ -22,160 +22,166 @@
  * @subpackage Wp_Vouched_Verify/public
  * @author     Brian Cole <https://github.com/bcolemutech>
  */
-class Wp_Vouched_Verify_Public {
+class Wp_Vouched_Verify_Public
+{
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string $plugin_name The ID of this plugin.
-	 */
-	private $plugin_name;
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $plugin_name The ID of this plugin.
+     */
+    private $plugin_name;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string $version The current version of this plugin.
-	 */
-	private $version;
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $version The current version of this plugin.
+     */
+    private $version;
 
-	private $wp_wrapper;
+    private $wp_wrapper;
 
-	/** @var vouched_service */
-	private $vouched_service;
+    /** @var vouched_service */
+    private $vouched_service;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @param string $plugin_name The name of the plugin.
-	 * @param string $version The version of this plugin.
-	 * @param wp_wrapper_interface $wp_wrapper
-	 * @param vouched_service_interface $vouched_service
-	 *
-	 * @since    1.0.0
-	 */
-	public function __construct( string $plugin_name, string $version, wp_wrapper_interface $wp_wrapper, vouched_service_interface $vouched_service) {
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @param string $plugin_name The name of the plugin.
+     * @param string $version The version of this plugin.
+     * @param wp_wrapper_interface $wp_wrapper
+     * @param vouched_service_interface $vouched_service
+     *
+     * @since    1.0.0
+     */
+    public function __construct(string $plugin_name, string $version, wp_wrapper_interface $wp_wrapper, vouched_service_interface $vouched_service)
+    {
 
-		$this->plugin_name = $plugin_name;
-		$this->version     = $version;
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
 
-		$this->wp_wrapper = $wp_wrapper;
-		$this->vouched_service  = $vouched_service;
-	}
+        $this->wp_wrapper = $wp_wrapper;
+        $this->vouched_service = $vouched_service;
+    }
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
+    /**
+     * Register the stylesheets for the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_styles()
+    {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wp_Vouched_Verify_Loader as all the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wp_Vouched_Verify_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Wp_Vouched_Verify_Loader as all the hooks are defined
+         * in that particular class.
+         *
+         * The Wp_Vouched_Verify_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-vouched-verify-public.css', array(), $this->version );
-	}
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/wp-vouched-verify-public.css', array(), $this->version);
+    }
 
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
+    /**
+     * Register the JavaScript for the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts()
+    {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wp_Vouched_Verify_Loader as all the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wp_Vouched_Verify_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Wp_Vouched_Verify_Loader as all the hooks are defined
+         * in that particular class.
+         *
+         * The Wp_Vouched_Verify_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-vouched-verify-public.js', array( 'jquery' ), $this->version );
-	}
+        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/wp-vouched-verify-public.js', array('jquery'), $this->version);
+    }
 
-	/**
-	 * Intercept user registration, send Vouched invite using email address. Save return invite ID to user meta.
-	 *
-	 * @param int $user_id User's ID
-	 *
-	 * @throws Requests_Exception_HTTP
-	 * @throws Exception
-	 */
-	public function handle_user_register( int $user_id ) {
-		error_log( "User ID: " . $user_id, 4 );
-		error_log( "User Email: " . $_POST['email'], 4 );
+    /**
+     * Intercept user registration, send Vouched invite using email address. Save return invite ID to user meta.
+     *
+     * @param int $user_id User's ID
+     *
+     * @throws Requests_Exception_HTTP
+     * @throws Exception
+     */
+    public function handle_user_register(int $user_id)
+    {
+        error_log("User ID: " . $user_id, 4);
+        error_log("User Email: " . $_POST['email'], 4);
 
-		$inviteID = $this->vouched_service->send_invite();
+        $inviteID = $this->vouched_service->send_invite();
 
-		error_log( "POST succeeded invite ID: " . $inviteID, 4 );
+        error_log("POST succeeded invite ID: " . $inviteID, 4);
 
-		$this->wp_wrapper->add_user_meta( $user_id, 'inviteID', $inviteID, true );
-	}
+        $this->wp_wrapper->add_user_meta($user_id, 'inviteID', $inviteID, true);
+    }
 
-	/**
-	 * Use WP_User to get Invite from Vouched. Then validates user is unique.
-	 * Sets user access based on Invote status. Will send new Invite if ID not found.
-	 * Will bypass Invite checks if user has higher privleges.
-	 *
-	 * @param string $username
-	 * @param WP_User $user
-	 *
-	 * @throws Requests_Exception_HTTP
-	 */
-	public function handle_wp_login( string $username, WP_User $user ) {
-		$inviteId = $this->wp_wrapper->get_user_meta( $user->ID, "inviteID" );
+    /**
+     * Use WP_User to get Invite from Vouched. Then validates user is unique.
+     * Sets user access based on Invote status. Will send new Invite if ID not found.
+     * Will bypass Invite checks if user has higher privleges.
+     *
+     * @param string $username
+     * @param WP_User $user
+     *
+     * @throws Requests_Exception_HTTP
+     */
+    public function handle_wp_login(string $username, WP_User $user)
+    {
+        $inviteId = $this->wp_wrapper->get_user_meta($user->ID, "inviteID");
 
-		error_log( "Retrived Invite " . $inviteId . " for user " . $user->ID, 4 );
+        error_log("Retrived Invite " . $inviteId . " for user " . $user->ID, 4);
 
-		$invite = $this->vouched_service->get_invite( $inviteId );
+        $invite = $this->vouched_service->get_invite($inviteId);
 
-		error_log( "Invite status: " . $invite->{'status'}, 4 );
+        error_log("Invite status: " . $invite->{'status'}, 4);
 
-		if ( $invite->{'status'} != 'completed' ) {
-			$this
-				->wp_wrapper
-				->add_user_meta( $user->ID, 'vouched-message', 'Vouched verification is not complete: Invite '. $invite->{'status'}, false );
+        if ($invite->{'status'} != 'completed') {
+            $this
+                ->wp_wrapper
+                ->add_user_meta($user->ID, 'vouched-message', 'Vouched verification is not complete: Invite ' . $invite->{'status'}, false);
 
             return;
-		}
+        }
 
         $job = $this->vouched_service->get_job($invite->{'jobId'});
 
         $success = $job->{'reviewSuccess'} ? 'true' : 'false';
-        error_log( "Job status: " . $job->{'status'} . ' | Review passed: ' . $success, 4 );
+        error_log("Job status: " . $job->{'status'} . ' | Review passed: ' . $success, 4);
 
-        if ( $job->{'status'} != 'completed' ) {
+        if ($job->{'status'} != 'completed') {
             $this
                 ->wp_wrapper
-                ->add_user_meta( $user->ID, 'vouched-message', 'Vouched verification is not complete: Job '. $job->{'status'}, false );
+                ->add_user_meta($user->ID, 'vouched-message', 'Vouched verification is not complete: Job ' . $job->{'status'}, false);
 
             return;
         }
 
-        if ( $job->{'reviewSuccess'} == false ) {
+        if ($job->{'reviewSuccess'} == false) {
             $this
                 ->wp_wrapper
-                ->add_user_meta( $user->ID, 'vouched-message', 'Verification review did not pass. See Invite for details '. $invite->{'url'}, false );
+                ->add_user_meta($user->ID, 'vouched-message', 'Verification review did not pass. See Invite for details ' . $invite->{'url'}, false);
 
             return;
         }
 
-	}
+    }
 }
--- test/LoginTest.php
+++ test/LoginTest.php
@@ -11,70 +11,70 @@ require_once dirname(__FILE__) . '/../src/services/vouched_service_interface.php
 
 class LoginTest extends TestCase
 {
-	/**
-	 * @throws Requests_Exception_HTTP
-	 */
-	public function testUserLoginShouldGetStatusOfInviteFromApi_HappyPath()
-	{
-		$stub = $this->getWpStub('completed', 'completed');
-		$stub->expects($this->once())->method('get_user_meta');
+    /**
+     * @throws Requests_Exception_HTTP
+     */
+    public function testUserLoginShouldGetStatusOfInviteFromApi_HappyPath()
+    {
+        $stub = $this->getWpStub('completed', 'completed');
+        $stub->expects($this->once())->method('get_user_meta');
 
-		$vouched = $this->getVouchedStub('completed', 'completed', true);
-		$vouched->expects($this->once())->method('get_invite')->with(123);
-		$vouched->expects($this->once())->method('get_job')->with(5);
+        $vouched = $this->getVouchedStub('completed', 'completed', true);
+        $vouched->expects($this->once())->method('get_invite')->with(123);
+        $vouched->expects($this->once())->method('get_job')->with(5);
 
         $stub->expects($this->never())
             ->method('add_user_meta');
 
-		$pluginPublic = new Wp_Vouched_Verify_Public('Wp_Vouched_Verify', '1.0.0', $stub, $vouched);
+        $pluginPublic = new Wp_Vouched_Verify_Public('Wp_Vouched_Verify', '1.0.0', $stub, $vouched);
 
-		$user = $this->createStub(WP_User::class);
+        $user = $this->createStub(WP_User::class);
 
-		$user->user_email = 'john@test.com';
-		$user->ID = 1;
+        $user->user_email = 'john@test.com';
+        $user->ID = 1;
 
-		$pluginPublic->handle_wp_login('John', $user);
-	}
-
-	/**
-	 * @throws Requests_Exception_HTTP
-	 */
-	public function testGivenInviteIsNotCompletedStoreMessage()
-	{
-		$stub = $this->getWpStub('accepted','active');
-		$stub->expects($this->once())->method('get_user_meta');
-		$stub->expects($this->any())
-		     ->method('wp_remote_get')
-		     ->with('test.com/api/invites/?id=123');
-		$stub->expects($this->once())
-		     ->method('add_user_meta')
-		     ->with(1,'vouched-message', 'Vouched verification is not complete: Invite accepted', false);
-
-		$vouched = $this->getVouchedStub('accepted', 'active', false);
-
-		$pluginPublic = new Wp_Vouched_Verify_Public('Wp_Vouched_Verify', '1.0.0', $stub, $vouched);
-
-		$user = $this->createStub(WP_User::class);
-
-		$user->user_email = 'john@test.com';
-		$user->ID = 1;
-
-		$pluginPublic->handle_wp_login('John', $user);
-	}
+        $pluginPublic->handle_wp_login('John', $user);
+    }
 
     /**
      * @throws Requests_Exception_HTTP
      */
-    public function testGivenInviteIsCompletedWhenJobIsNotThenStoreMessage()
+    public function testGivenInviteIsNotCompletedStoreMessage()
     {
-        $stub = $this->getWpStub('completed','active');
+        $stub = $this->getWpStub('accepted', 'active');
         $stub->expects($this->once())->method('get_user_meta');
         $stub->expects($this->any())
             ->method('wp_remote_get')
             ->with('test.com/api/invites/?id=123');
         $stub->expects($this->once())
             ->method('add_user_meta')
-            ->with(1,'vouched-message', 'Vouched verification is not complete: Job active', false);
+            ->with(1, 'vouched-message', 'Vouched verification is not complete: Invite accepted', false);
+
+        $vouched = $this->getVouchedStub('accepted', 'active', false);
+
+        $pluginPublic = new Wp_Vouched_Verify_Public('Wp_Vouched_Verify', '1.0.0', $stub, $vouched);
+
+        $user = $this->createStub(WP_User::class);
+
+        $user->user_email = 'john@test.com';
+        $user->ID = 1;
+
+        $pluginPublic->handle_wp_login('John', $user);
+    }
+
+    /**
+     * @throws Requests_Exception_HTTP
+     */
+    public function testGivenInviteIsCompletedWhenJobIsNotThenStoreMessage()
+    {
+        $stub = $this->getWpStub('completed', 'active');
+        $stub->expects($this->once())->method('get_user_meta');
+        $stub->expects($this->any())
+            ->method('wp_remote_get')
+            ->with('test.com/api/invites/?id=123');
+        $stub->expects($this->once())
+            ->method('add_user_meta')
+            ->with(1, 'vouched-message', 'Vouched verification is not complete: Job active', false);
 
         $vouched = $this->getVouchedStub('completed', 'active', false);
 
@@ -93,14 +93,14 @@ class LoginTest extends TestCase
      */
     public function testGivenInviteIsCompletedAndJobIsCompletedWhenReviewSuccessIsFalseThenStoreMessage()
     {
-        $stub = $this->getWpStub('completed','completed');
+        $stub = $this->getWpStub('completed', 'completed');
         $stub->expects($this->once())->method('get_user_meta');
         $stub->expects($this->any())
             ->method('wp_remote_get')
             ->with('test.com/api/invites/?id=123');
         $stub->expects($this->once())
             ->method('add_user_meta')
-            ->with(1,'vouched-message', 'Verification review did not pass. See Invite for details test.com/invite/123', false);
+            ->with(1, 'vouched-message', 'Verification review did not pass. See Invite for details test.com/invite/123', false);
 
         $vouched = $this->getVouchedStub('completed', 'completed', false);
 
@@ -114,28 +114,30 @@ class LoginTest extends TestCase
         $pluginPublic->handle_wp_login('John', $user);
     }
 
-	/**
-	 * @return mixed|MockObject|wp_wrapper_interface
-	 */
-	public function getWpStub(string $inviteStatus, string $jobStatus) {
-		$stub = $this->getMockBuilder( wp_wrapper_interface::class )->getMock();
-		$stub->method( 'get_option' )->willReturn( array( 'url' => 'test.com', 'api_key' => 'abcd1234' ) );
+    /**
+     * @return mixed|MockObject|wp_wrapper_interface
+     */
+    public function getWpStub(string $inviteStatus, string $jobStatus)
+    {
+        $stub = $this->getMockBuilder(wp_wrapper_interface::class)->getMock();
+        $stub->method('get_option')->willReturn(array('url' => 'test.com', 'api_key' => 'abcd1234'));
 
-		$stub->method( 'get_user_meta' )->willReturn( '123' );
-		$stub->method( 'wp_remote_get' )->with('test.com/api/invites/?id=123')
-		     ->willReturn( array('body' => "{\"invite\":[{\"id\":\"123\",\"jobId\":\"5\",\"status\":\"" . $inviteStatus . "\"}]}" ));
+        $stub->method('get_user_meta')->willReturn('123');
+        $stub->method('wp_remote_get')->with('test.com/api/invites/?id=123')
+            ->willReturn(array('body' => "{\"invite\":[{\"id\":\"123\",\"jobId\":\"5\",\"status\":\"" . $inviteStatus . "\"}]}"));
 
-		$stub->method('wp_remote_get')->with('test.com/api/jobs/?id=5')
-			->willReturn(array('body'=>"{\"items\":[{\"id\":\"5\",\"status\":\"".$jobStatus."\"}]}"));
-		return $stub;
-	}
+        $stub->method('wp_remote_get')->with('test.com/api/jobs/?id=5')
+            ->willReturn(array('body' => "{\"items\":[{\"id\":\"5\",\"status\":\"" . $jobStatus . "\"}]}"));
+        return $stub;
+    }
 
-	private function getVouchedStub(string $inviteStatus, string $jobStatus, bool $reviewSuccess) {
-		$stub = $this->getMockBuilder(vouched_service_interface::class)->getMock();
-		$stub->method('get_invite')->with('123')
-			->willReturn((object) array('status'=> $inviteStatus, 'jobId'=>'5', 'url' => 'test.com/invite/123'));
+    private function getVouchedStub(string $inviteStatus, string $jobStatus, bool $reviewSuccess)
+    {
+        $stub = $this->getMockBuilder(vouched_service_interface::class)->getMock();
+        $stub->method('get_invite')->with('123')
+            ->willReturn((object)array('status' => $inviteStatus, 'jobId' => '5', 'url' => 'test.com/invite/123'));
         $stub->method('get_job')->with('5')
-            ->willReturn((object) array('status'=> $jobStatus, 'reviewSuccess'=> $reviewSuccess));
-		return $stub;
-	}
+            ->willReturn((object)array('status' => $jobStatus, 'reviewSuccess' => $reviewSuccess));
+        return $stub;
+    }
 }
