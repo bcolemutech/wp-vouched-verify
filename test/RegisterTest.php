fcb1b7b0c773d6656ececc1364ba4f32369c0027
bcolemutech <bcolemutech@hotmail.com>
2021-11-14 14:05:12 -0600

    Alter test to use services

--- test/RegisterTest.php
+++ test/RegisterTest.php
@@ -4,47 +4,37 @@ use PHPUnit\Framework\TestCase;
 
 require_once dirname(__FILE__) . '/../src/public/class-wp-vouched-verify-public.php';
 require_once dirname(__FILE__) . '/../src/services/wp_wrapper_interface.php';
+require_once dirname(__FILE__) . '/../src/services/vouched_service_interface.php';
+require_once dirname(__FILE__) . '/../src/services/user_service_interface.php';
 
 class RegisterTest extends TestCase
 {
+    /**
+     * @throws Requests_Exception_HTTP
+     */
     public function testUserRegisterShouldSaveInviteId()
     {
         $stub = $this->getWpStub();
 
-        $vouched = $this->getVouchedStub();
+        $vouched = $this->getMockBuilder(vouched_service_interface::class)->getMock();
 
-        $stub
-            ->expects($this->once())
-            ->method('add_user_meta')
-            ->with(
-                $this->equalTo(1),
-                $this->equalTo('inviteID'),
-                $this->equalTo('123'),
-                $this->equalTo(true)
-            );
+        $vouched->expects($this->once())->method('send_invite')->will($this->returnValue('123'));
 
         $userService = $this->getMockBuilder(user_service_interface::class)->getMock();
+
+        $userService->expects($this->once())->method('set_invite_id')->with($this->equalTo(1), $this->equalTo('123'));
 
         $pluginPublic = new Wp_Vouched_Verify_Public('Wp_Vouched_Verify', '1.0.0', $stub, $vouched, $userService);
         $_POST['email'] = "test";
         $pluginPublic->handle_user_register(1);
     }
 
-    /**
-     * @return mixed
-     */
+
     private function getWpStub()
     {
         $stub = $this->getMockBuilder(wp_wrapper_interface::class)->getMock();
 
-        $stub->method('get_option')->willReturn(array('url' => 'test.com', 'api_key' => 'abcd1234'));
-        return $stub;
-    }
-
-    private function getVouchedStub() {
-        $stub = $this->getMockBuilder(vouched_service_interface::class)->getMock();
-        $stub->method('send_invite')
-            ->willReturn('123');
+        $stub->method('get_option')->willReturn(array('url' => 'test.com', 'api_key' => 'abc1234'));
         return $stub;
     }
 }
