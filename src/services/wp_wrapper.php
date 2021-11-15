6b183a3cae516726a558db595aea6680b027cb24
bcolemutech <bcolemutech@hotmail.com>
2021-11-14 21:55:28 -0600

    - method to run user meta queries - formatting

--- src/services/wp_wrapper.php
+++ src/services/wp_wrapper.php
@@ -1,34 +1,51 @@
 <?php
 
-class wp_wrapper implements wp_wrapper_interface {
+class wp_wrapper implements wp_wrapper_interface
+{
 
-	public function get_option( string $string ) {
-		return get_option( $string );
-	}
+    public function get_option(string $string)
+    {
+        return get_option($string);
+    }
 
-	public function wp_remote_post( string $url, array $args ): array {
-		$response = wp_remote_post( $url, $args );
+    /**
+     * @throws Exception
+     */
+    public function wp_remote_post(string $url, array $args): array
+    {
+        $response = wp_remote_post($url, $args);
 
-		if ( is_wp_error( $response ) ) {
-			throw new Exception( $response->get_error_message() );
-		}
+        if (is_wp_error($response)) {
+            throw new Exception($response->get_error_message());
+        }
 
-		return $response;
-	}
+        return $response;
+    }
 
-	public function wp_remote_retrieve_response_code( array $response ): int {
-		return wp_remote_retrieve_response_code( $response );
-	}
+    public function wp_remote_retrieve_response_code(array $response): int
+    {
+        return wp_remote_retrieve_response_code($response);
+    }
 
-	public function add_user_meta( int $user_id, string $meta_key, $meta_value, bool $unique ): bool {
-		return add_user_meta( $user_id, $meta_key, $meta_value, $unique );
-	}
+    public function add_user_meta(int $user_id, string $meta_key, $meta_value, bool $unique): bool
+    {
+        return add_user_meta($user_id, $meta_key, $meta_value, $unique);
+    }
 
-	public function get_user_meta( int $user_id, string $meta_key ): string {
-		return get_user_meta( $user_id, $meta_key, true );
-	}
+    public function get_user_meta(int $user_id, string $meta_key): string
+    {
+        return get_user_meta($user_id, $meta_key, true);
+    }
 
-	public function wp_remote_get( string $url, array $args ): array {
-		return wp_remote_get( $url, $args );
-	}
+    public function wp_remote_get(string $url, array $args): array
+    {
+        return wp_remote_get($url, $args);
+    }
+
+    public function query_users_with_meta_query(array $query): array
+    {
+        $user_query = new WP_User_Query($query);
+
+        return $user_query->get_results();
+    }
 }
